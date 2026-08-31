502a9daa078e3f0893d6426f9ac418df31559086
przish <irish_may_pureza@dlsl.edu.ph>
2026-09-01 03:09:29 +0800

    basics

---
+++ review.py
@@ -0,0 +1,22 @@
+print("Hello, world!", end=" ")
+print("i love you")
+
+print("I am", 25)
+# this is how you print string + int
+# this is also how you comment
+
+# Write a single-line comment
+
+# Comment out this line so it does not run:
+# This is a comment
+# print("This should not run")
+
+# Add a multiline comment
+
+"""
+This is
+a multiline
+comment
+"""
+
+
