25cfd0b6920ad5b1f9cedd623d223c753578f316
przish <irish_may_pureza@dlsl.edu.ph>
2026-09-01 03:20:04 +0800

    printing and basic variable manipulations

--- review.py
+++ review.py
@@ -20,3 +20,47 @@ comment
 """
 
 
+# you can name your variable anything but number as first character, hyphen, whitespace
+# vars are case sensitive 
+
+"""
+camel helloWorld
+pascal HelloWorld
+snake hello_world
+"""
+
+# assigning multivalues
+
+x, y, z = 1, 2, 3
+
+print(x)
+print(y)
+print(z)
+
+print(x, y, z)
+
+# one value to many variables
+x = y = z = "hello"
+
+print(x, y, z)
+
+# unpacking
+
+fruits = ["apple", "banana", "cherry"]
+
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
+print(x, y, z)
+
+# if automatic merong space, use comma. if not, you can use +. or if you want to combine different data types without encountering errors, use comma.
+# using comma is most recommended
+
+x = 5
+y = "John"
+
+# print(x + y) output: unsupported operand type(s) for +: 'int' and 'str'
+print(x, y)
+
